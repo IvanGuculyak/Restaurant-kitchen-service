3d99cba4cf851dbb0e46037fc4e50f8f1427f44b
IvanGuculyak <ivan.guculyak@gmail.com>
2023-06-22 18:37:04 +0300

    Added static file styles.css

--- kitchen/urls.py
+++ kitchen/urls.py
@@ -14,6 +14,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
@@ -23,4 +25,4 @@ urlpatterns = [
         "service/",
         include("service.urls", namespace="service")
     )
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
