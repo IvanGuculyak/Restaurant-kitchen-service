a628515502e83881451a0611185771523247b5ce
IvanGuculyak <ivan.guculyak@gmail.com>
2023-06-29 14:14:01 +0300

    added requirements.txt and updated .env, settings.py, and readme.md files

--- kitchen/settings.py
+++ kitchen/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 import os
 from pathlib import Path
+from dotenv import load_dotenv
+
+load_dotenv()
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -22,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", "django-insecure-ojev81r6!yncr&(io#($ukuassu!f-&i$a5)%5m#eqzf4q0y2-")
+SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", "SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
